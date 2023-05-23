cbe21aa772d508ae73fa18bdaba129fcc8e2b46c
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-05-23 20:30:38 +0530

    Header-completed

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import People from "../../assets/people.png"
+import Ai from "../../assets/ai.png"
+import "./header.css"
 
 const Header = () => {
   return (
-    <h1>Header</h1>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+        The first version of GPT was released in 2018 and contained 117 million parameters. The second version of the model, GPT-2, was released in 2019 with around 1.5 billion parameters. As the latest version, GPT-3 jumps over the last model by a huge margin with over 175 billion parameters, which is more than 100 times its predecessor and ten times more than comparable programs.
+        </p>
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your Email Address'/>
+          <button type='button'>Get Started</button>
+        </div>
+        <div className='gpt3__header-content__people'>
+          <img src={People} alt="People"/>
+          <p>1,600 People requested access in last 24 hours</p>
+
+        </div>
+      </div>
+        <div className='gpt3__header-image'>
+          <img src={Ai} alt='ai'/>
+        </div>
+    </div>
   )
 }
 
--- src/container/whatgpt3/WhatGpt3.jsx
+++ src/container/whatgpt3/WhatGpt3.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Feature } from '../../components'
+import "./whatgpt3.css"
 
 const WhatGpt3 = () => {
   return (
-    <div>WhatGpt3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='whatgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature/>
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Explore The Library
+        </p>
+        <div className='gpt3__whatgpt3-container'>
+          <Feature/>
+          <Feature/>
+          <Feature/>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
